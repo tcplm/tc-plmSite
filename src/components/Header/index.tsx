0c8b9c44034ea5edd83662b36180ed8421b87fad
Tc <savdekar.d@gmail.com>
2024-11-15 16:13:37 +0530

    Update index.tsx
    build error was there

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -158,7 +158,8 @@ const Header = () => {
                   </ul>
                 </nav>
               </div>
-              {/* <div className="flex items-center justify-end pr-16 lg:pr-0">
+              {/*
+              <div className="flex items-center justify-end pr-16 lg:pr-0">
                 <Link
                   href="/signin"
                   className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
@@ -171,7 +172,8 @@ const Header = () => {
                 >
                   Sign Up
                 </Link>
-                <div> */}
+                <div>
+                  /*}
                   <ThemeToggler />
                 </div>
               </div>
